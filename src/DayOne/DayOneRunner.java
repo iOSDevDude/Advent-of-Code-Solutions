3a74a2b0d1409dad4d5e848907c261fb2c36de9a
benosowiecki <benjamin.osowiecki@gmail.com>
2020-12-20 20:58:46 -0600

    Adds Day One Solution

---
+++ src/DayOne/DayOneRunner.java
@@ -0,0 +1,29 @@
+package DayOne;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class DayOneRunner {
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //Read input and transform to integers
+        Scanner input = new Scanner(new File("src/DayOne/expense_report.txt")).useDelimiter("\n");
+        ArrayList<Integer> numbers = new ArrayList();
+        while(input.hasNext()) {
+            numbers.add(Integer.valueOf(input.next()));
+        }
+
+
+        numbers.forEach(firstNum -> {
+              numbers.forEach(secondNum -> {
+                  if(firstNum+secondNum == 2020) {
+                      System.out.println(firstNum*secondNum);
+                  }
+              });
+        });
+
+    }
+}
