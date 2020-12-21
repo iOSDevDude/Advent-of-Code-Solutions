a0dd1d9ee45027f234893e7181f22e99b396ade1
benosowiecki <benjamin.osowiecki@gmail.com>
2020-12-20 23:23:34 -0600

    Not yet working Day 4 Part 1

--- src/Day4/DayFourRunner.java
+++ src/Day4/DayFourRunner.java
@@ -1,21 +1,51 @@
 package Day4;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class DayFourRunner {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
         //Read input and transform to integers
-        Scanner input = new Scanner(new File("src/Day3/tree_map.txt")).useDelimiter("\n");
-        ArrayList<String> treeMap = new ArrayList<>();
+        Scanner input = new Scanner(new File("src/Day4/passport_records.txt")).useDelimiter(" |\\n");
+        ArrayList<String> passportDataList = new ArrayList<>();
         while(input.hasNext()) {
-            treeMap.add(input.next());
+            passportDataList.add(input.next());
         }
+
+        ArrayList<Map<String, String>> passportList = passportListTransformer(passportDataList);
+
+        partOne(passportList);
+
     }
 
-    static void partOne() {
+    static void partOne(ArrayList<Map<String, String>> passports) {
+        long validPassportCount = passports.stream()
+                .filter(p -> p.get("byr") != null && p.get("iyr") != null && p.get("eyr") != null && p.get("hgt") != null && p.get("hcl") != null && p.get("ecl") != null && p.get("pid") != null)
+                .count();
 
+        System.out.println("Part One: " + validPassportCount);
+    }
+
+    static ArrayList<Map<String, String>> passportListTransformer(ArrayList<String> passportDataList) {
+        ArrayList<Map<String, String>> passportList = new ArrayList<>();
+
+        HashMap<String, String> passport = new HashMap<>();
+        int index = 0;
+        for(String element : passportDataList) {
+            index++;
+            if(element.length() == 0 || index == passportDataList.size()) {
+                passportList.add(passport);
+                passport = new HashMap<>();
+            } else {
+                passport.put(element.substring(0, element.indexOf(":")), element.substring(element.indexOf(":")+1));
+            }
+        }
+
+        return passportList;
     }
 }
