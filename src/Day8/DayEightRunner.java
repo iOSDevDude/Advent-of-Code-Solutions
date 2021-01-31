d43f039fe43c60a48c00efe07d649148d5c463b4
benosowiecki <benjamin.osowiecki@gmail.com>
2021-01-31 00:42:53 -0600

    Adds solution to Day 8 Part 2

--- src/Day8/DayEightRunner.java
+++ src/Day8/DayEightRunner.java
@@ -3,36 +3,57 @@ package Day8;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class DayEightRunner {
 
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner input = new Scanner(new File("src/Day8/instructions.txt")).useDelimiter("\n");
-        List<String> instructions = new ArrayList<>();
-        while(input.hasNext()) {
-            instructions.add(input.next());
-        }
-
-        for(String instruction : instructions) {
-            System.out.println(instruction);
-        }
-
-        List<Instruction> parsedInstructions = parseInstructions(instructions);
-
-        System.out.println("Part One: " + partOne(parsedInstructions));
+        System.out.println("Part One: " + partOne(parseInstructions()));
+        System.out.println("Part Two: " + partTwo(parseInstructions()));
     }
 
     public static int partOne(List<Instruction> instructions) {
+        return runInstructions(instructions).accumulator;
+    }
+
+    public static int partTwo(List<Instruction> instructions) throws FileNotFoundException{
+
+        List<Instruction> instructionsCopy = new ArrayList<>(instructions);
+
+        for(int i=0; i<instructions.size(); i++) {
+            Instruction instruction = instructionsCopy.get(i);
+            if(InstructionType.valueOf(instruction.operation) == InstructionType.acc) {
+                continue;
+            }
+
+            switch (InstructionType.valueOf(instruction.operation)) {
+                case jmp:
+                    instruction.operation = InstructionType.nop.name();
+                    break;
+                case nop:
+                    instruction.operation = InstructionType.jmp.name();
+            }
+
+            RunResult result = runInstructions(instructionsCopy);
+            if(result.terminatedSuccessfully) {
+                return result.accumulator;
+            } else {
+                instructionsCopy = parseInstructions();
+            }
+        }
+
+        return 0;
+    }
+
+    public static RunResult runInstructions(List<Instruction> instructions) {
         int accumulator = 0;
 
         for(int i=0; i<instructions.size(); i++) {
             Instruction instruction = instructions.get(i);
 
             if(instruction.executed) {
-                return accumulator;
+                return new RunResult(false, accumulator);
             }
 
             switch (InstructionType.valueOf(instruction.operation)) {
@@ -45,14 +66,19 @@ public class DayEightRunner {
                 case nop:
                     break;
             }
-            instructions.get(i).setExecuted(true);
-            System.out.println(instructions.get(i).executed);
+            instructions.get(i).executed = true;
         }
 
-        return accumulator;
+        return new RunResult(true, accumulator);
     }
 
-    public static List<Instruction> parseInstructions(List<String> rawInstructions) {
+    public static List<Instruction> parseInstructions() throws FileNotFoundException {
+        Scanner input = new Scanner(new File("src/Day8/instructions.txt")).useDelimiter("\n");
+        List<String> rawInstructions = new ArrayList<>();
+        while(input.hasNext()) {
+            rawInstructions.add(input.next());
+        }
+
         List<Instruction> instructions = new ArrayList<>();
 
         for(String rawInstruction : rawInstructions) {
@@ -79,9 +105,15 @@ public class DayEightRunner {
             this.argument = argument;
             this.executed = false;
         }
+    }
 
-        public void setExecuted(Boolean executed) {
-            this.executed = executed;
+    private static class RunResult {
+        Boolean terminatedSuccessfully;
+        Integer accumulator;
+
+        public RunResult(Boolean terminatedSuccessfully, Integer accumulator) {
+            this.terminatedSuccessfully = terminatedSuccessfully;
+            this.accumulator = accumulator;
         }
     }
 }
